53fcd883ceac632fa9f651b50688323c23ddc688
LONKA JAYANTH REDDY <jayanthreddy1704@gmail.com>
2026-02-28 10:41:18 +0000

    second commit

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using node js not browser 
+console.log(3+3);
+console.log("jayanth");
+
+let age=18
+let name="jayanth"
+let account=true
+
+console.log(typeof age)
+
+//number=>range 2 to power 53
+//bigint
+//string=>" " or ' '
+//boolean=>true or false
+//undefined ex:let name
+//null=>special cse ex:let temp=null
+//symbol=>unique
+
+//object(important)
---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=1342
+let accountEmail="jayanth@123gmail.com"
+var accountPassword="12345"
+accountCity="hyderabad"
+let accountState;
+
+//";" is not mandatory in js
+/*
+var is old method so dont use 
+and it has issue block and functional scope 
+*/
+
+accountEmail="jayanth@gmail.com"
+accountCity="Chennai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
