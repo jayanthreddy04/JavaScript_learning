f7890c287d011c77d3f5c93cc738245f6bf62ca1
LONKA JAYANTH REDDY <jayanthreddy1704@gmail.com>
2026-03-01 07:10:03 +0000

    comparison and coversions

---
+++ basics/comparison.js
@@ -0,0 +1,15 @@
+console.log(2>=3);//f
+console.log("2">1)//t
+console.log("2">"1");//t
+
+console.log(null==0);//f
+console.log(null>0);//f
+console.log(null>=0);//t
+
+console.log(undefined>0);//f
+console.log(undefined==0);//f
+console.log(undefined>=0);//f
+
+
+
+
---
+++ basics/conversionOperation.js
@@ -0,0 +1,64 @@
+/*let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score)
+console.log(typeof value);
+ console.log(value);
+
+ //"33"=>33
+ //"33abc"=>NaN
+ //undefined=>NaN
+ //null=>0
+ //"jay"=>NaN
+ //true=>1  false=>0
+
+ let isLoggedin=1
+ let Booleanval=Boolean(isLoggedin)
+ console.log(Booleanval);
+
+ //1=>true 0=>false
+ //""=>false 
+ //"jay"=>true 
+
+let a=33
+let s=String(a)
+console.log(s);
+console.log(typeof s)*/
+
+//*************************OPERATIONS******************************//
+ 
+let value=5
+let ngvalue=-value
+// console.log(ngvalue);
+
+// console.log(2+2)
+// console.log(5-2);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(2%2);
+// console.log(2**3);
+
+// let s1="hello"
+// let s2="jayanth"
+// let s3=s1+s2
+// console.log(s3);
+
+// console.log("1"+2)//12
+// console.log(1+"2")//12
+// console.log("1"+2+2)//122
+// console.log(1+2+"2");//32
+
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(+"");//0
+
+let val=100
+val++
+console.log(val);
+
+
+
+
+
+
